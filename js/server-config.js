1e2d866be21afe05e17cb5cc409b0ae916b60d6f
amraed55 <sgalamrahmed@gmail.com>
2026-05-25 23:50:58 +0300

    Add files via upload

---
+++ js/server-config.js
@@ -0,0 +1,9 @@
+window.SERVER_CONFIG = {
+  centralSyncUrls: [
+    'https://jsonblob.com/api/jsonBlob/019e5019-833f-7d5c-870b-6f8d25c82b83'
+  ],
+  localFallbackPath: 'data/central-sync-default.json',
+  cacheStorageKey: 'cloudAppCentralSnapshotCache.v1',
+  remoteProjectShellUrl: 'https://amrrahm.github.io/abnh/',
+  remoteProjectPollMs: 15000
+};
