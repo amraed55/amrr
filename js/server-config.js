663f027c610465d531ca712a38a46c1f45a774ef
amraed55 <sgalamrahmed@gmail.com>
2026-05-25 23:45:53 +0300

    Delete js directory

--- js/server-config.js
+++
@@ -1,9 +0,0 @@
-window.SERVER_CONFIG = {
-  centralSyncUrls: [
-    'https://jsonblob.com/api/jsonBlob/019e5ba7-7767-73b7-8484-617b3031174d'
-  ],
-  localFallbackPath: 'data/central-sync-default.json',
-  cacheStorageKey: 'cloudAppCentralSnapshotCache.v1',
-  remoteProjectShellUrl: 'https://amrrahm.github.io/abnh/',
-  remoteProjectPollMs: 15000
-};
